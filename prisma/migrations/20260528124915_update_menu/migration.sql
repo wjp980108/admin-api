74db6dc3d37f397fffa38bd09da9f625b86bd48d
wjp <wjp_github@163.com>
2026-05-28 21:36:05 +0800

    revert(prisma): 撤销历时记录

--- prisma/migrations/20260528124915_update_menu/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `activeMenu` on the `meuns` table. The data in that column could be lost. The data in that column will be cast from `TinyInt` to `VarChar(191)`.
-
-*/
--- AlterTable
-ALTER TABLE `meuns` MODIFY `activeMenu` VARCHAR(191) NULL;
