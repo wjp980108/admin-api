a0fdd42add8715d795dd3763a0af574cd51c296d
wjp <wjp_github@163.com>
2026-06-29 23:09:02 +0800

    fix(middlewares): [rateLimit] 减少限流时间窗口到3分钟

--- src/middlewares/rateLimitMiddlewares.ts
+++ src/middlewares/rateLimitMiddlewares.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 // 全局限流
 export const globalLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 时间窗口：15 分钟（单位：毫秒）
+  windowMs: 3 * 60 * 1000, // 时间窗口：15 分钟（单位：毫秒）
   max: 100, // 同一 IP 在时间窗口内最多请求 100 次，超出后触发限流
   message: {
     code: 429,
@@ -12,10 +12,10 @@ export const globalLimiter = rateLimit({
 
 // 登录接口限流
 export const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 3 * 60 * 1000,
   max: 10,
   message: {
     code: 429,
-    message: '登录尝试过于频繁，请15分钟后再试',
+    message: '登录尝试过于频繁，请3分钟后再试',
   },
 });
